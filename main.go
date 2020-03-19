7ab71b5d1b3b2564818c0a71b223705a48d27718
Joonas Myhrberg <joonas@myhrberg.email>
2020-03-19 23:50:24 +0200

    Move command generation to specific files

--- main.go
+++ main.go
@@ -14,56 +14,7 @@ func main() {
 		Usage:   "A CLI for HR Flow",
 		Before:  checkConfig,
 		Commands: []*cli.Command{
-			{
-				Name:  "report",
-				Usage: "add a new hour report",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:    "duration",
-						Aliases: []string{"d"},
-						Usage:   "`DURATION` to report, formatted as 8h30m. Will be ignored if both start and end time are defined.",
-						Value:   "8h",
-					},
-					&cli.TimestampFlag{
-						Name:        "start",
-						Aliases:     []string{"s"},
-						Layout:      "15:04",
-						Usage:       "Set workday start to `TIME`.",
-						DefaultText: "now - DURATION",
-					},
-					&cli.TimestampFlag{
-						Name:        "end",
-						Aliases:     []string{"e"},
-						Layout:      "15:04",
-						Usage:       "Set workday end to `TIME`.",
-						DefaultText: "now",
-					},
-					&cli.StringFlag{
-						Name:        "project",
-						Aliases:     []string{"p"},
-						Usage:       "which `PROJECT` to assign to the report.",
-						DefaultText: "none",
-					},
-					&cli.StringFlag{
-						Name:        "comment",
-						Aliases:     []string{"c"},
-						Usage:       "assign a `COMMENT` to the report.",
-						DefaultText: "empty",
-					},
-					&cli.TimestampFlag{
-						Name:        "date",
-						Layout:      "2.1.",
-						Usage:       "`DATE` for the report, format 'd.M.' (years not supported)",
-						DefaultText: "today",
-					},
-					&cli.BoolFlag{
-						Name:  "hourly",
-						Value: false,
-						Usage: "Report units as an hourly worker, also won't include 30 minute lunch in the duration.",
-					},
-				},
-				Action: report,
-			},
+			reportCommandFactory(),
 		},
 		EnableBashCompletion: true,
 	}
