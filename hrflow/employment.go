f6ba404c9ca0a9b2cabf6f365b0605ee1d897332
Joonas Myhrberg <joonas@myhrberg.email>
2020-11-20 17:28:54 +0200

    Add commenting to Employment

--- hrflow/employment.go
+++ hrflow/employment.go
@@ -1,19 +1,22 @@
 package hrflow
 
 type Employment struct {
-	IsPassive              bool     `json:"isPassive"`
-	IsDefaultEmployment    bool     `json:"isDefaultEmployment"`
-	EmploymentID           int64    `json:"employmentId"`
-	PersonID               int64    `json:"personId"`
-	Name                   string   `json:"name"`
-	ListName               string   `json:"listName"`
-	GroupID                int64    `json:"groupId"`
-	GroupName              string   `json:"groupName"`
-	ParentGroupID          int64    `json:"parentGroupId"`
-	CustomerID             int64    `json:"customerId"`
-	UserName               string   `json:"userName"`
-	StartDate              string   `json:"startDate"`
-	EndDate                *string  `json:"endDate"`
+	IsPassive           bool  `json:"isPassive"`
+	IsDefaultEmployment bool  `json:"isDefaultEmployment"`
+	EmploymentID        int64 `json:"employmentId"`
+	PersonID            int64 `json:"personId"`
+	// Name of the employment, contains some ID, name of the person etc.
+	Name string `json:"name"`
+	// ListName is the name of the person to be used when listing employees, Lastname Firstname style.
+	ListName      string  `json:"listName"`
+	GroupID       int64   `json:"groupId"`
+	GroupName     string  `json:"groupName"`
+	ParentGroupID int64   `json:"parentGroupId"`
+	CustomerID    int64   `json:"customerId"`
+	UserName      string  `json:"userName"`
+	StartDate     string  `json:"startDate"`
+	EndDate       *string `json:"endDate"`
+	// EnterpriseName is the official name of the company.
 	EnterpriseName         string   `json:"enterpriseName"`
 	AllEmploymentIDs       *[]int64 `json:"allEmploymentIds"`
 	ValueSettings          string   `json:"valueSettings"`
